3a9b0ab548a2e4c00a7e31b53f5d13b3dbf615d2
Fabian <frakxman@hotmail.com>
2025-02-17 18:15:32 -0500

    Add types and validatos to products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,8 +1,23 @@
+import { IsString, IsNotEmpty, IsNumber, IsArray, IsBoolean, IsPositive } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty() 
   name: string;
+  @IsString()
+  @IsNotEmpty()
   description: string;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   price: number;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   stock: number;
+  @IsArray()
+  @IsNotEmpty()
   images: string[];
+  @IsBoolean()
   status: boolean;
 }
--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -1,34 +1,100 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { UpdateProductDto } from '../dto/update-product.dto';
 
+import { Product } from '../entities/product.entity';
+
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
+  private products: Product[] = [
+    {
+      id: '1',
+      name: 'Product 1',
+      price: 100,
+      description: 'Description 1',
+      images: [
+        'https://picsum.photos/640/640?r=1',
+        'https://picsum.photos/640/640?r=2',
+        'https://picsum.photos/640/640?r=3'
+      ],
+      stock: 10,
+      status: true,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 200,
+      description: 'Description 2',
+      images: [
+        'https://picsum.photos/640/640?r=4',
+        'https://picsum.photos/640/640?r=5',
+        'https://picsum.photos/640/640?r=6'
+      ],
+      stock: 20,
+      status: true,
+    },
+    {
+      id: '3',
+      name: 'Product 3',
+      price: 300,
+      description: 'Description 3',
+      images: [
+        'https://picsum.photos/640/640?r=7',
+        'https://picsum.photos/640/640?r=8',
+        'https://picsum.photos/640/640?r=9'
+      ],
+      stock: 30,
+      status: true,
+    },
+  ];
+
+  create(createProductDto: CreateProductDto) { 
+    const id = this.products.length + 1;
+    const product: Product = {
+      id: id.toString(),
+      name: createProductDto.name,
+      price: createProductDto.price,
+      description: createProductDto.description,
+      images: createProductDto.images,
+      stock: createProductDto.stock,
+      status: createProductDto.status,
+    };
+    this.products.push(product);
     return {
       message: 'Product created successfully',
-      product: createProductDto,
+      product,
       status: HttpStatus.CREATED,
     };
   }
 
   findAll() {
+    if (this.products.length === 0) {
+      throw new HttpException('No products found', HttpStatus.NOT_FOUND);
+    }
     return {
       message: 'Products fetched successfully',
-      products: [],
+      products: this.products,
       status: HttpStatus.OK,
     };
   }
 
   findOne(id: string) {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+      throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+    }
     return {
       message: 'Product fetched successfully',
-      product: {},
+      product,
       status: HttpStatus.OK,
     };
   }
 
   update(id: string, updateProductDto: UpdateProductDto) {
+    const product = this.findOne(id);
+    if (!product) {
+      throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+    }
     return {
       message: 'Product updated successfully',
       product: updateProductDto,
@@ -37,6 +103,11 @@ export class ProductsService {
   }
 
   remove(id: string) {
+    const product = this.findOne(id);
+    if (!product) {
+      throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+    }
+    this.products = this.products.filter((product) => product.id !== id);
     return {
       message: 'Product removed successfully',
       status: HttpStatus.OK,
